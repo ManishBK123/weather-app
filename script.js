7bc8c2c919c4787a1886cb96f51a623d3cda9824
ManishBK123 <manishsuanr59@gmail.com>
2025-01-08 15:07:42 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const btn=document.querySelector(".btn");
+const cityInput=document.querySelector("#city")
+const temp=document.querySelector("#temp-div")
+const apikey ="c6210fe974ac87ecd26fb6a6276043f5"
+
+btn.addEventListener("click", async function(){
+    let city =cityInput.value;
+    const weatherdata=await getWeatherInfo(city)
+    console.log(weatherdata.main,temp)
+    temp.innerHTML=weatherdata.main.temp+""
+})
+
+    async function getWeatherInfo(city)
+    {
+const apikey ="c6210fe974ac87ecd26fb6a6276043f5"
+        const apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`
+
+        
+            const response= await fetch(apiUrl);
+            const data= await response();
+            return data;}
+        
+    //     const response= await fetch(apiUrl);
+    //     const data= response.json()
+    //     console.log(data)
+    //     console.log(city)
+    // }
+    // getWeatherInfo("butwal")
